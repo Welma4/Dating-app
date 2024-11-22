a9e54ded90aa038279536701c7bbb79b6c1f3710
BoryaZhivotnoe <ozzymozzy34@gmail.com>
2024-11-22 19:08:42 +0300

    default screen
    изменил отображение default screen

--- DatingApp/app/src/main/java/com/example/datingapp/ui/screens/HomeScreen.kt
+++ DatingApp/app/src/main/java/com/example/datingapp/ui/screens/HomeScreen.kt
@@ -5,6 +5,7 @@ import android.annotation.SuppressLint
 import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -21,9 +22,11 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
@@ -83,7 +86,6 @@ fun HomeScreen(
     }
 }
 
-
 @Composable
 fun VerticalSwipeFeed(
     items: List<UserEntity>,
@@ -118,72 +120,142 @@ fun VerticalSwipeFeed(
             painter = painterResource(id = R.drawable.home_app_logo),
             contentDescription = "home_app_logo",
             modifier = Modifier
-
                 .padding(vertical = 40.dp)
                 .fillMaxWidth(0.4f)
                 .height(36.dp)
         )
-
 
         VerticalPager(
             count = Int.MAX_VALUE,
             state = pagerState,
             modifier = Modifier
                 .fillMaxHeight(0.9f)
-                .shadow(
-                    elevation = 16.dp,
-                    shape = RoundedCornerShape(30.dp),
-                ),
+                .clip(RoundedCornerShape(16.dp)),
             itemSpacing = 30.dp,
         ) { virtualPage ->
             val actualPage = virtualPage % itemCount
             val user = items[actualPage]
             val userPhoto = photoBitmaps.value[user.uid]
+
             Box(
                 modifier = Modifier
                     .fillMaxWidth()
                     .fillMaxHeight()
+                    .clip(RoundedCornerShape(16.dp))
             ) {
-                if (userPhoto != null) {
-                    Image(
-                        bitmap = userPhoto.asImageBitmap(),
-                        contentDescription = "user_photo",
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .clip(RoundedCornerShape(30.dp)),
-                        contentScale = ContentScale.Crop
-                    )
-                } else {
-                    Image(
-                        painter = painterResource(id = R.drawable.default_icon),
-                        contentDescription = "default_photo",
-                        modifier = Modifier
-                            .fillMaxSize()
-                            .clip(RoundedCornerShape(30.dp)),
-                        contentScale = ContentScale.Crop
-                    )
-                }
-                Column(
+                Box(
                     modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(horizontal = 16.dp, vertical = 15.dp)
+                        .fillMaxSize()
+                        .background(
+                            brush = Brush.verticalGradient(
+                                colors = listOf(Color(0xFF7512B2), Color(0xFFBD94D7))
+                            )
+                        )
+                )
+
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(16.dp))
                 ) {
-                    Text(
-                        text = "${items[actualPage].firstName}  ${items[actualPage].secondName}, ${calculateAge(items[actualPage].birthDate!!)}",
-                        fontSize = 28.sp,
-                        fontWeight = FontWeight.Bold,
-                        color = Color.White,
-                    )
-                    Text(
-                        text = items[actualPage].location,
-                        fontSize = 14.sp,
-                        fontWeight = FontWeight.Normal,
-                        color = Color.White,
-                    )
+                    if (userPhoto != null) {
+                        // Верхняя тень
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.TopCenter)
+                                .fillMaxWidth()
+                                .height(10.dp)
+                                .shadow(10.dp, shape = RectangleShape, clip = true)
+                        )
+
+                        // Левая тень
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.CenterStart)
+                                .fillMaxHeight()
+                                .width(10.dp)
+                                .shadow(10.dp, shape = RectangleShape, clip = true)
+                        )
+
+                        // Правая тень
+                        Box(
+                            modifier = Modifier
+                                .align(Alignment.CenterEnd)
+                                .fillMaxHeight()
+                                .width(10.dp)
+                                .shadow(10.dp, shape = RectangleShape, clip = true)
+                        )
+
+                        Image(
+                            bitmap = userPhoto.asImageBitmap(),
+                            contentDescription = "user_photo",
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .clip(RoundedCornerShape(16.dp)),
+                            contentScale = ContentScale.Crop
+                        )
+                    } else {
+                        Image(
+                            painter = painterResource(id = R.drawable.default_icon),
+                            contentDescription = "default_photo",
+                            modifier = Modifier
+                                .fillMaxSize(0.2f)
+                                .clip(RoundedCornerShape(16.dp)),
+                            contentScale = ContentScale.Crop
+                        )
+                    }
                 }
 
+                Column(
+                    modifier = Modifier
+                        .align(Alignment.BottomStart)
+                        .padding(horizontal = 16.dp, vertical = 15.dp)
+                ) {
+                    val textStyle = androidx.compose.ui.text.TextStyle(
+                        fontSize = 28.sp,
+                        fontWeight = FontWeight.Bold
+                    )
+
+                    Box {
+                        Text(
+                            text = "${items[actualPage].firstName}  ${items[actualPage].secondName}, ${calculateAge(items[actualPage].birthDate!!)}",
+                            style = textStyle,
+                            color = Color.Black,
+                            modifier = Modifier.offset(x = 2.dp, y = 2.dp)
+                        )
+                        Text(
+                            text = "${items[actualPage].firstName}  ${items[actualPage].secondName}, ${calculateAge(items[actualPage].birthDate!!)}",
+                            style = textStyle,
+                            color = Color.White
+                        )
+                    }
+
+                    val locationStyle = androidx.compose.ui.text.TextStyle(
+                        fontSize = 16.sp,
+                        fontWeight = FontWeight.Normal
+                    )
+
+                    Box {
+                        Text(
+                            text = items[actualPage].location,
+                            style = locationStyle,
+                            color = Color.Black,
+                            modifier = Modifier.offset(x = 2.dp, y = 2.dp)
+                        )
+                        Text(
+                            text = items[actualPage].location,
+                            style = locationStyle,
+                            color = Color.White
+                        )
+                    }
+                }
             }
         }
     }
 }
+
+
+
+
+
 
