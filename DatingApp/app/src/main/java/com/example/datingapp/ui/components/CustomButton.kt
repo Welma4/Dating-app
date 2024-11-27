6832ad6dba6be34109aa4671e2e7b6faaaf28d85
BoryaZhivotnoe <ozzymozzy34@gmail.com>
2024-11-27 18:10:56 +0300

    добавил компонент кнопки, сверстал LikeScreen

---
+++ DatingApp/app/src/main/java/com/example/datingapp/ui/components/CustomButton.kt
@@ -0,0 +1,41 @@
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import androidx.compose.ui.unit.TextUnit
+import com.example.datingapp.ui.theme.poppinsFontFamily
+
+
+@Composable
+fun CustomButton(
+    text: String,
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    backgroundColor: Color = Color(0xFFAA3FEC),
+    textColor: Color = Color.White,
+    textSize: TextUnit = 16.sp
+) {
+    Button(
+        onClick = onClick,
+        colors = ButtonDefaults.buttonColors(containerColor = backgroundColor),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(65.dp)
+            .padding(bottom = 10.dp)
+    ) {
+        Text(
+            text = text,
+            fontFamily = poppinsFontFamily,
+            fontWeight = FontWeight.Normal,
+            fontSize = 16.sp,
+            color = Color.White
+        )
+    }
+}
