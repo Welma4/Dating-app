f7c15c667c37af934807e10ce669ff5f8e5c20f4
Welma4 <imortallwelma@gmail.com>
2024-11-30 15:41:08 +0300

    Анимация лайка и сохранения лайка в бд

---
+++ DatingApp/app/src/main/java/com/example/datingapp/ui/components/VerticalSwipeFeed.kt
@@ -0,0 +1,216 @@
+import android.graphics.Bitmap
+import android.util.Log
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.gestures.detectHorizontalDragGestures
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.draw.shadow
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.datingapp.R
+import com.example.datingapp.data.LikeEntity
+import com.example.datingapp.data.UserEntity
+import com.example.datingapp.ui.theme.LikePink
+import com.example.datingapp.ui.theme.LikePinkAlpha
+import com.example.datingapp.ui.theme.MediumGray
+import com.example.datingapp.ui.utils.calculateAge
+import com.example.datingapp.viewmodel.LikeViewModel
+import com.example.datingapp.viewmodel.PhotoViewModel
+import com.google.accompanist.pager.VerticalPager
+import com.google.accompanist.pager.rememberPagerState
+
+@Composable
+fun VerticalSwipeFeed(
+    items: List<UserEntity>,
+    photoViewModel: PhotoViewModel,
+    likeViewModel: LikeViewModel,
+    currentUserId: String
+) {
+    val pagerState = rememberPagerState()
+    val itemCount = items.size
+    val photoBitmaps = remember { mutableStateOf<Map<String, Bitmap>>(emptyMap()) }
+
+    LaunchedEffect(items) {
+        items.forEach { user ->
+            photoViewModel.getPhotoBitmap(
+                idUser = user.uid,
+                onSuccess = { bitmap ->
+                    photoBitmaps.value = photoBitmaps.value + (user.uid to bitmap)
+                },
+                onFailure = { error ->
+                    Log.e("VerticalSwipeFeed", "Error loading photo for user ${user.uid}: $error")
+                }
+            )
+        }
+    }
+
+    VerticalPager(
+        count = Int.MAX_VALUE,
+        state = pagerState,
+        modifier = Modifier
+            .fillMaxHeight(0.92f)
+            .padding(horizontal = 16.dp),
+        itemSpacing = 30.dp,
+    ) { virtualPage ->
+        val actualPage = virtualPage % itemCount
+        val user = items[actualPage]
+        val userPhoto = photoBitmaps.value[user.uid]
+
+        var dragOffset by remember { mutableStateOf(0f) }
+        var hasLiked by remember { mutableStateOf(false) }
+
+        val filterAlpha by animateFloatAsState(
+            targetValue = if (hasLiked) 0.3f else (dragOffset / 300f).coerceIn(0f, 0.3f),
+            animationSpec = tween(durationMillis = 200)
+        )
+        val showHeartIcon = filterAlpha >= 0.3f
+
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .fillMaxHeight()
+                .pointerInput(Unit) {
+                    detectHorizontalDragGestures(
+                        onHorizontalDrag = { _, dragAmount ->
+                            if (!hasLiked) {
+                                dragOffset = (dragOffset + dragAmount).coerceAtLeast(0f)
+                                if (dragOffset >= 300f) {
+                                    hasLiked = true
+                                    dragOffset = 300f
+
+                                    likeViewModel.saveLikeToFirestore(
+                                        LikeEntity(
+                                            currentUserId,
+                                            user.uid
+                                        ),
+                                        onSuccess = {
+                                            Log.d("MyTag", "Like saved successfully")
+                                        },
+                                        onFailure = { error ->
+                                            Log.d("MyTag", error)
+                                        }
+                                    )
+                                }
+                            }
+                        },
+                        onDragEnd = {
+                            if (!hasLiked) {
+                                dragOffset = 0f
+                            }
+                        }
+                    )
+                }
+        ) {
+            if (userPhoto != null) {
+                Image(
+                    bitmap = userPhoto.asImageBitmap(),
+                    contentDescription = "user_photo",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .shadow(
+                            elevation = 8.dp,
+                            shape = RoundedCornerShape(30.dp),
+                            spotColor = Color.Black,
+                            ambientColor = Color.Black
+                        )
+                        .clip(RoundedCornerShape(30.dp)),
+                    contentScale = ContentScale.Crop
+                )
+            } else {
+                Image(
+                    painter = painterResource(id = R.drawable.default_icon),
+                    contentDescription = "default_photo",
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .shadow(
+                            elevation = 8.dp,
+                            shape = RoundedCornerShape(30.dp),
+                            spotColor = Color.Black,
+                            ambientColor = Color.Black
+                        )
+                        .clip(RoundedCornerShape(30.dp)),
+                    contentScale = ContentScale.Crop
+                )
+            }
+
+            if (filterAlpha > 0f) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .clip(RoundedCornerShape(30.dp))
+                        .background(LikePinkAlpha)
+                        .alpha(filterAlpha)
+                )
+            }
+
+            if (showHeartIcon) {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .wrapContentSize(align = Alignment.Center)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_heart),
+                        contentDescription = "Heart Icon",
+                        modifier = Modifier
+                            .size(100.dp)
+                            .alpha(1f),
+                        tint = LikePink
+                    )
+                }
+            }
+
+            Column(
+                modifier = Modifier
+                    .align(Alignment.BottomStart)
+                    .padding(horizontal = 16.dp, vertical = 15.dp)
+            ) {
+                Text(
+                    text = "${user.firstName}  ${user.secondName}, ${calculateAge(user.birthDate!!)}",
+                    fontSize = 28.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = Color.White,
+                    style = TextStyle(shadow = Shadow(MediumGray, Offset(5.0f, 2.0f), 1.0f))
+                )
+                Text(
+                    text = user.location,
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight.Normal,
+                    color = Color.White,
+                    style = TextStyle(shadow = Shadow(MediumGray, Offset(5.0f, 2.0f), 1.0f))
+                )
+            }
+        }
+    }
+}
--- DatingApp/app/src/main/java/com/example/datingapp/ui/screens/HomeScreen.kt
+++ DatingApp/app/src/main/java/com/example/datingapp/ui/screens/HomeScreen.kt
@@ -1,19 +1,17 @@
 package com.example.datingapp.ui.screens
 
 import ProfileViewModel
+import VerticalSwipeFeed
 import android.annotation.SuppressLint
-import android.graphics.Bitmap
 import android.util.Log
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 import com.example.datingapp.ui.components.NavigationMenu
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.runtime.LaunchedEffect
@@ -23,28 +21,17 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.draw.shadow
-import androidx.compose.ui.geometry.Offset
-import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.Shadow
-import androidx.compose.ui.graphics.asImageBitmap
-import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.text.TextStyle
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.datingapp.R
 import com.example.datingapp.data.UserEntity
 import com.example.datingapp.ui.components.FiltersDialog
-import com.example.datingapp.ui.theme.MediumGray
-import com.example.datingapp.ui.utils.calculateAge
+import com.example.datingapp.ui.components.HomeHeaderSection
+import com.example.datingapp.ui.components.LoadingState
 import com.example.datingapp.viewmodel.GenderViewModel
+import com.example.datingapp.viewmodel.LikeViewModel
 import com.example.datingapp.viewmodel.PhotoViewModel
 import com.example.datingapp.viewmodel.PreferencesViewModel
-import com.google.accompanist.pager.*
-import com.google.firebase.auth.FirebaseAuth
 
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
@@ -55,6 +42,7 @@ fun HomeScreen(
     photoViewModel: PhotoViewModel,
     preferencesViewModel: PreferencesViewModel,
     genderViewModel: GenderViewModel,
+    likeViewModel: LikeViewModel,
     currentUserId: String
 ) {
     val usersList = remember { mutableStateOf<List<UserEntity>>(emptyList()) }
@@ -97,12 +85,14 @@ fun HomeScreen(
             bottomBar = { NavigationMenu(navController) },
         ) {
             Column(modifier = Modifier.fillMaxSize()) {
-                HeaderSection(isFilterMenuVisible)
+                HomeHeaderSection(isFilterMenuVisible)
 
                 if (usersList.value.isNotEmpty()) {
                     VerticalSwipeFeed(
                         items = usersList.value.map { it },
                         photoViewModel = photoViewModel,
+                        likeViewModel = likeViewModel,
+                        currentUserId = currentUserId,
                     )
                 } else {
                     LoadingState()
@@ -129,130 +119,6 @@ fun HomeScreen(
     }
 }
 
-@Composable
-fun HeaderSection(isFilterMenuVisible: MutableState<Boolean>) {
-    Row(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(horizontal = 16.dp, vertical = 35.dp),
-        verticalAlignment = Alignment.CenterVertically
-    ) {
-        Spacer(modifier = Modifier.weight(0.25f))
 
-        Image(
-            painter = painterResource(id = R.drawable.home_app_logo),
-            contentDescription = "home_app_logo",
-            modifier = Modifier
-                .weight(2f)
-                .height(36.dp)
-        )
 
-        IconButton(
-            modifier = Modifier.size(30.dp),
-            onClick = { isFilterMenuVisible.value = true }
-        ) {
-            Icon(
-                modifier = Modifier.size(30.dp),
-                painter = painterResource(id = R.drawable.ic_filter),
-                contentDescription = "filter"
-            )
-        }
-    }
-}
 
-@Composable
-fun VerticalSwipeFeed(
-    items: List<UserEntity>,
-    photoViewModel: PhotoViewModel,
-) {
-    val pagerState = rememberPagerState()
-    val itemCount = items.size
-    val photoBitmaps = remember { mutableStateOf<Map<String, Bitmap>>(emptyMap()) }
-
-    LaunchedEffect(items) {
-        items.forEach { user ->
-            photoViewModel.getPhotoBitmap(
-                idUser = user.uid,
-                onSuccess = { bitmap ->
-                    photoBitmaps.value = photoBitmaps.value + (user.uid to bitmap)
-                },
-                onFailure = { error ->
-                    Log.e("VerticalSwipeFeed", "Error loading photo for user ${user.uid}: $error")
-                }
-            )
-        }
-    }
-
-    VerticalPager(
-        count = Int.MAX_VALUE,
-        state = pagerState,
-        modifier = Modifier
-            .fillMaxHeight(0.92f)
-            .padding(horizontal = 16.dp),
-        itemSpacing = 30.dp,
-    ) { virtualPage ->
-        val actualPage = virtualPage % itemCount
-        val user = items[actualPage]
-        val userPhoto = photoBitmaps.value[user.uid]
-
-        Box(
-            modifier = Modifier
-                .fillMaxWidth()
-                .fillMaxHeight()
-        ) {
-            if (userPhoto != null) {
-                Image(
-                    bitmap = userPhoto.asImageBitmap(),
-                    contentDescription = "user_photo",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .shadow(elevation = 8.dp, shape = RoundedCornerShape(30.dp), spotColor = Color.Black, ambientColor = Color.Black)
-                        .clip(RoundedCornerShape(30.dp)),
-                    contentScale = ContentScale.Crop
-                )
-            } else {
-                Image(
-                    painter = painterResource(id = R.drawable.default_icon),
-                    contentDescription = "default_photo",
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .shadow(elevation = 8.dp, shape = RoundedCornerShape(30.dp), spotColor = Color.Black, ambientColor = Color.Black)
-                        .clip(RoundedCornerShape(30.dp)),
-                    contentScale = ContentScale.Crop
-                )
-            }
-
-            Column(
-                modifier = Modifier
-                    .align(Alignment.BottomStart)
-                    .padding(horizontal = 16.dp, vertical = 15.dp)
-            ) {
-                Text(
-                    text = "${items[actualPage].firstName}  ${items[actualPage].secondName}, ${calculateAge(items[actualPage].birthDate!!)}",
-                    fontSize = 28.sp,
-                    fontWeight = FontWeight.Bold,
-                    color = Color.White,
-                    style = TextStyle(shadow = Shadow(MediumGray, Offset(5.0f, 2.0f), 1.0f))
-                )
-                Text(
-                    text = items[actualPage].location,
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.Normal,
-                    color = Color.White,
-                    style = TextStyle(shadow = Shadow(MediumGray, Offset(5.0f, 2.0f), 1.0f))
-                )
-            }
-        }
-    }
-}
-
-@Composable
-fun LoadingState() {
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ) {
-        Text(text = "Loading users...", fontSize = 40.sp)
-    }
-}
