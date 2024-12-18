46a6a0f034a1034575767de1144ab32447a2b3f7
Welma4 <imortallwelma@gmail.com>
2024-12-18 13:29:10 +0300

    Реализовал экран с чатами, теперь корректно отображаются все созданные в бд чаты для текущего пользователя

--- DatingApp/app/src/main/java/com/example/datingapp/ui/screens/MessagesScreen.kt
+++ DatingApp/app/src/main/java/com/example/datingapp/ui/screens/MessagesScreen.kt
@@ -1,17 +1,25 @@
 package com.example.datingapp.ui.screens
 
+import ProfileViewModel
 import android.annotation.SuppressLint
+import android.graphics.Bitmap
 import android.util.Log
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateMapOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -20,22 +28,80 @@ import androidx.navigation.NavController
 import com.example.datingapp.data.ChatEntity
 import com.example.datingapp.ui.components.NavigationMenu
 import com.example.datingapp.viewmodel.ChatViewModel
+import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.runtime.*
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.asImageBitmap
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.datingapp.R
+import com.example.datingapp.ui.theme.MediumPink
+import com.example.datingapp.viewmodel.PhotoViewModel
 
 @SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun MessagesScreen(
     navController: NavController,
     chatViewModel: ChatViewModel,
+    profileViewModel: ProfileViewModel,
+    photoViewModel: PhotoViewModel,
     currentUserId: String
 ) {
-
     val chatList = chatViewModel.chatList.observeAsState(emptyList())
+    val userNameMap = remember { mutableStateMapOf<String, String>() }
+    val userPhotoMap = remember { mutableStateMapOf<String, Bitmap?>() }
+    var isLoading by remember { mutableStateOf(true) }
 
     LaunchedEffect(currentUserId) {
         chatViewModel.fetchChatsForUser(
             userId = currentUserId,
-            onSuccess = {},
-            onFailure = { error -> Log.d("MyTag", error) }
+            onSuccess = {
+                chatList.value.forEach { chat ->
+                    val userIdToFetch =
+                        if (chat.idFirstUser == currentUserId) chat.idSecondUser else chat.idFirstUser
+                    if (userIdToFetch !in userNameMap) {
+                        profileViewModel.fetchUserFromFirestore(
+                            uid = userIdToFetch,
+                            onSuccess = { user ->
+                                userNameMap[userIdToFetch] = user.firstName
+                                if (userNameMap.size == chatList.value.size) {
+                                    isLoading = false
+                                }
+                            },
+                            onFailure = { error ->
+                                Log.e("MessagesScreen", "Error fetching user: $error")
+                                isLoading = false
+                            }
+                        )
+                    }
+
+                    if (userIdToFetch !in userPhotoMap) {
+                        photoViewModel.getPhotoBitmap(
+                            idUser = userIdToFetch,
+                            onSuccess = { bitmap ->
+                                userPhotoMap[userIdToFetch] = bitmap
+                            },
+                            onFailure = { error ->
+                                Log.e("MessagesScreen", "Error fetching photo: $error")
+                                userPhotoMap[userIdToFetch] = null
+                            }
+                        )
+                    }
+                }
+                if (chatList.value.isEmpty()) {
+                    isLoading = false
+                }
+            },
+            onFailure = { error ->
+                Log.e("MessagesScreen", "Error fetching chats: $error")
+                isLoading = false
+            }
         )
     }
 
@@ -43,67 +109,149 @@ fun MessagesScreen(
         modifier = Modifier.fillMaxSize(),
         bottomBar = { NavigationMenu(navController) }
     ) {
-        if (chatList.value.isNotEmpty()) {
-            ChatList(chatList.value)
-        } else {
-            Text("No chats available.", modifier = Modifier.padding(16.dp))
-        }
-    }
-}
-
-@Composable
-fun ChatList(chatList: List<ChatEntity>) {
-    LazyColumn(
-        modifier = Modifier.padding(8.dp),
-        contentPadding = PaddingValues(bottom = 16.dp)
-    ) {
-        items(chatList) { chat ->
-            ChatItem(chat)
-        }
-    }
-}
-
-@Composable
-fun ChatItem(chat: ChatEntity) {
-    // Карточка для каждого чата
-    Card(
-        modifier = Modifier
-            .fillMaxWidth()
-            .padding(8.dp),
-    ) {
         Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxWidth()
+            modifier = Modifier.fillMaxSize()
         ) {
-            // idSecondUser - Верхняя строка
-            Text(
-                text = "Chat with: ${chat.idSecondUser}",
-                style = androidx.compose.material3.MaterialTheme.typography.bodyLarge,
-                color = Color.Black
-            )
-
-            // lastMessage - под idSecondUser
-            Text(
-                text = chat.lastMessageId.ifEmpty { "No messages yet" },
-                style = androidx.compose.material3.MaterialTheme.typography.bodyMedium,
-                color = Color.Gray,
-                modifier = Modifier.padding(top = 4.dp)
-            )
-
-            Row(
+            Box(
                 modifier = Modifier
                     .fillMaxWidth()
-                    .padding(top = 8.dp),
-                horizontalArrangement = Arrangement.SpaceBetween,
-                verticalAlignment = Alignment.CenterVertically
+                    .padding(start = 16.dp, end = 16.dp, top = 24.dp, bottom = 4.dp)
             ) {
+                IconButton(
+                    onClick = { navController.popBackStack() },
+                    modifier = Modifier.align(Alignment.CenterStart)
+                ) {
+                    Icon(
+                        painter = painterResource(id = R.drawable.ic_back_arrow),
+                        contentDescription = "Back Arrow",
+                        tint = Color.Black,
+                        modifier = Modifier.size(32.dp)
+                    )
+                }
+
                 Text(
-                    text = "Last update: ${chat.lastUpdateTime}",
-                    style = androidx.compose.material3.MaterialTheme.typography.bodySmall,
-                    color = Color.Gray
+                    text = "Chat",
+                    fontSize = 20.sp,
+                    fontWeight = FontWeight.Bold,
+                    modifier = Modifier.align(Alignment.Center)
                 )
+            }
+            if (isLoading) {
+                Box(
+                    modifier = Modifier.fillMaxSize(),
+                    contentAlignment = Alignment.Center
+                ) {
+                    CircularProgressIndicator(
+                        modifier = Modifier.size(200.dp),
+                        color = MediumPink
+                    )
+                }
+            } else {
+                if (chatList.value.isNotEmpty()) {
+                    ChatList(chatList.value, currentUserId, userNameMap, userPhotoMap)
+                } else {
+                    Text("No chats available.", modifier = Modifier.padding(16.dp))
+                }
             }
         }
     }
 }
+
+@Composable
+fun ChatList(
+    chatList: List<ChatEntity>,
+    currentUserId: String,
+    userNameMap: Map<String, String>,
+    userPhotoMap: Map<String, Bitmap?>
+) {
+    LazyColumn(
+        modifier = Modifier.padding(8.dp).background(color = Color.Transparent),
+        contentPadding = PaddingValues(bottom = 16.dp)
+    ) {
+        items(chatList) { chat ->
+            ChatItem(chat, currentUserId, userNameMap, userPhotoMap)
+        }
+    }
+}
+
+@Composable
+fun ChatItem(
+    chat: ChatEntity,
+    currentUserId: String,
+    userNameMap: Map<String, String>,
+    userPhotoMap: Map<String, Bitmap?>
+) {
+    val userIdToShow =
+        if (chat.idFirstUser == currentUserId) chat.idSecondUser else chat.idFirstUser
+    val userName = userNameMap[userIdToShow] ?: "Loading..."
+    val userPhoto = userPhotoMap[userIdToShow]
+
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .height(65.dp),
+        colors = CardDefaults.cardColors(containerColor = Color.Transparent)
+    ) {
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            if (userPhoto != null) {
+                Image(
+                    bitmap = userPhoto.asImageBitmap(),
+                    contentDescription = "User photo",
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .aspectRatio(1f)
+                        .clip(CircleShape),
+                    contentScale = ContentScale.Crop
+                )
+            } else {
+                Box(
+                    modifier = Modifier
+                        .fillMaxHeight()
+                        .aspectRatio(1f)
+                        .clip(CircleShape)
+                        .background(Color.Gray),
+                    contentAlignment = Alignment.Center
+                ) {
+                    Text("N/A", color = Color.White)
+                }
+            }
+
+            Spacer(modifier = Modifier.width(16.dp))
+
+            Column(
+                modifier = Modifier.fillMaxHeight(0.8f),
+                verticalArrangement = Arrangement.SpaceBetween
+            ) {
+                Text(
+                    text = userName,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                )
+                Row(
+                    modifier = Modifier.fillMaxSize(),
+                    horizontalArrangement = Arrangement.SpaceBetween,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = chat.lastMessageId.ifEmpty { "Type your first message!" },
+                        color = Color.Gray,
+                        fontSize = 14.sp,
+                        maxLines = 1,
+                        overflow = TextOverflow.Ellipsis
+                    )
+                    Text(
+                        text = chat.lastUpdateTime,
+                        color = Color.Gray,
+                        fontSize = 12.sp
+                    )
+                }
+            }
+        }
+    }
+}
+
+
